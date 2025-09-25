1af277990883756fc8390fb234db7316bc73a8ae
Henrique Santos <henrique.santos@recife.ifpe.edu.br>
2025-09-25 15:14:56 -0300

    Novo site preview (#9)
    * Preview do novo site
    
    * Primeira versão para teste

---
+++ preview/assets/js/data/awards.js
@@ -0,0 +1,26 @@
+const awards = [
+    {
+        year: 2022,
+        pubs: [
+            {category: "Mostra Tecnológica", title: "Primeiro Lugar  - XIII Congresso Norte Nordeste de Pesquisa e Inovação (CONNEPI): TelemetricSys - Sistema Telemétrico Dinâmico Autônomo e Suas Aplicações", link: "#", linkTitle: "2022 - Registro INPI"},
+        ]
+    }, 
+    {
+        year: 2021,
+        pubs: [
+            {category: "Congresso", title: "Primeiro Lugar - Categoria Tecnologia, Região Nordeste - Prêmio PI nas Escolas", link: "#", linkTitle: "INPI"},
+        ]
+    }, 
+    {
+        year: 2018,
+        pubs: [
+            {category: "Mostra Tecnológica", title: "Primeiro Lugar - XII Congresso Norte Nordeste de Pesquisa e Inovação (CONNEPI): Desenvolvimento de Módulos das Estações para aplicação no Sistema Telemétrico RailBee", link: "#", linkTitle: "Instituto Federal de Pernambuco e Instituto Federal do Sertão Pernambucano"},
+        ]
+    }, 
+    {
+        year: 2016,
+        pubs: [
+            {category: "Mostra Tecnológica", title: "Primeiro Lugar - XI Congresso Norte Nordeste de Pesquisa e Inovação (CONNEPI): Desenvolvimento de módulos roteadores e finais remotos para aplicação no Sistema Telemétrico RailBee", link: "#", linkTitle: "Instituto Federal de Alagoas"},
+        ]
+    }, 
+];
